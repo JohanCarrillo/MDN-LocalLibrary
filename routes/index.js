eb7e5bffa0312f45e83159dbf9787faf339f7eb2
JohanCarrillo <johanime1907@gmail.com>
2022-07-21 11:01:05 -0500

    starting part 4 of the project

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+// doesnt have next() because is sending/rendering a response
+
+module.exports = router;
